8ff1e509ea1dfe488e7c2b5a222789117cfffebb
Gaurang Raval <129145863+GaurangRavalG@users.noreply.github.com>
2025-08-04 12:20:18 +0530

    commit

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:task/Call/Call.dart';
+import 'package:task/Form/Form.dart';
+import 'package:task/Task/CommonScreen.dart';
+import 'Assignments/Task1.dart';
+import 'Assignments/Task2.dart';
+
+void main() {
+  runApp(MaterialApp(home: FormScreen(),debugShowCheckedModeBanner: false));
+}
